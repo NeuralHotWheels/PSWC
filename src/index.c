9ed710738efc4f7bcaeef9a8bd8abe71ba8ac4d4
Prabhanjana P Hegde <prabhanjanahegde@gmail.com>
2025-05-09 06:53:04 +0530

    GUI built
    everything works eqcept delete

--- src/index.c
+++ src/index.c
@@ -1,54 +1,28 @@
-#include<stdlib.h>
-#include<stdio.h>
-#include<string.h>
+#include <ncurses.h>
+#include <string.h>
+#include <stdlib.h>
 
-//[]->[]->[].. []->[]->NULL
-
-//task store in nodes
+//task
 struct node{
-    char name[50];
-    struct node*  next;
+        char name[45];
+        struct node*  next;
 };
 
-//output number of nodes
+//gives length of list
 int length(struct node* hPtr){
-    struct node* p= hPtr;
-    p= p->next;
-    int i= 1;
-    while(p!=NULL){
+        struct node* p= hPtr;
         p= p->next;
-        i++;
-    }
-    i--;
-    return i;
+        int i= 1;
+        while(p!=NULL){
+                p= p->next;
+                i++;
+        }
+        i--;
+        return i;
 }
 
-//hPtr: list head pointer
-//print name of nodes
-void print(struct node* hPtr){
-    if(length(hPtr)==0){
-        printf("no tasks added\n");
-        return;
-    }
-    struct node* p= hPtr;
-    p= p->next;
-    int i= 1;
-    printf("---START---\n");
-    while(p!=NULL){
-        printf("%d  %s\n",i, p->name);
-        p= p->next;
-        i++;
-    }
-    printf("---END---\n");
-}
 
-//NOTE: NODES ARE 1 INDEXED 
-/*
-add node
-priority->index 
-name of node-> nm
-*/
-void add(int index, char nm[50], struct node* hPtr){
+void add(int index, char nm[45], struct node* hPtr){
     if(length(hPtr)+1<index){
         return;
     }
@@ -64,7 +38,7 @@ void add(int index, char nm[50], struct node* hPtr){
 
 /*
 delete node
-priority->index 
+priority->index
 */
 void del(int index, struct node* hPtr){
     if(length(hPtr)<index){
@@ -79,11 +53,6 @@ void del(int index, struct node* hPtr){
     free(old);
 }
 
-/*
-change priority node
-initial priority->i1 
-final priority-> i2
-*/
 void transfer(int i1, int i2, struct node* hPtr){
     int r= 0;
     if(i1==i2){
@@ -94,7 +63,7 @@ void transfer(int i1, int i2, struct node* hPtr){
     struct node* p= hPtr;
     for(int i= 1; i< i1; i++){
         p= p->next;
-    }
+        }
     p= p->next;
     struct node* q= hPtr;
     for(int i= 1; i< i2; i++){
@@ -104,81 +73,223 @@ void transfer(int i1, int i2, struct node* hPtr){
     del(i1+r,hPtr);
 }
 
-int main(){
-    //init list
-    struct node* head= (struct node*) malloc(sizeof(struct node));
-    char t[50];
-    strcpy(head->name, "null");
-    head->next= NULL;
+int main(int argc, char **argv){
 
-    char cmd[20];
-    //state loop
-    while(1) {
-        /*display loop commands
-            -tasks: displays all tasks in list
-            -add: add tasks
-        */
-        printf("COMMAND SET:\n-tasks\n-add\n-update\n-remove\n-quit\n--");
-        scanf("%s",cmd);
-        
-        // add new task
-        if(strcmp(cmd,"add")==0){
-            int pT;
-            char nT[50];
-            printf("enter task name: ");
-            scanf(" %49[^\n]",nT);
-            printf("enter task priority: ");
-            scanf("%d",&pT);
-            
-            if(pT>length(head)+1){
-                pT= length(head)+1;
-            }
-            add(pT,nT,head);
-            printf("task added\n");
+        //initialize list
+        struct node* head= (struct node*) malloc(sizeof(struct node));
+        head->next= NULL;
+
+        //initialize variables
+        int maxY, maxX;
+        int cursorX= 0;
+        int cursorY= 0;
+
+        //initialize main window;
+        initscr();
+        noecho();
+        getmaxyx(stdscr, maxY, maxX);
+        WINDOW * mainWin= newwin(maxY-2, 70, 2, 37);
+        refresh();
+        box(mainWin, 0, 0);
+        keypad(mainWin, true);
+        wrefresh(mainWin);
+
+        struct node* p= head;
+        p= p->next;
+        if(cursorX==0 & cursorY==0){
+             wattron(mainWin, A_REVERSE);
         }
-        //show task list
-        else if(strcmp(cmd,"tasks")==0){
-            print(head);
-            printf("len: %d\n",length(head));
+        mvwprintw(mainWin, 1,55,"add");
+        if(cursorX==0 & cursorY==0){
+                wattroff(mainWin, A_REVERSE);
         }
-        //update task
-        else if(strcmp(cmd,"update")==0){
-            print(head);
-            int pT1;
-            int pT2;
-            printf("enter priority of task: ");
-            scanf("%d",&pT1);
-            printf("enter new priority of task: ");
-            scanf("%d",&pT2);
-            if(pT1>length(head) | pT2>length(head)){
-                printf("index out of bound");
-            }
-            transfer(pT1,pT2,head);
-            print(head);
+
+        if(cursorX==1 & cursorY==0){
+                wattron(mainWin, A_REVERSE);
         }
-        //delete
-        else if(strcmp(cmd,"remove")==0){
-            int pT;
-            printf("enter priority of tasks to be removed: ");
-            scanf("%d",&pT);
-            if(pT>length(head)){
-                printf("task with priorty %d does not exist", pT);
-            }
-            else{
-                printf("task removed\n");
-            }
-            del(pT,head);
-            
+        mvwprintw(mainWin, 1,62,"quit");
+        if(cursorX==1 & cursorY==0){
+                wattroff(mainWin, A_REVERSE);
         }
-        //quit
-        else if(strcmp(cmd,"quit")==0){
-            printf("...exiting...");
-            break;
+        for(int i= 1; i<=length(head); i++){
+                if(cursorX==0 & cursorY==i){
+                        wattron(mainWin, A_REVERSE);
+                }
+                mvwprintw(mainWin, i+1,1,"%s",p->name);
+                if(cursorX==0 & cursorY==i){
+                        wattroff(mainWin, A_REVERSE);
+                }
+
+                if(cursorX==1 & cursorY==i){
+                        wattron(mainWin, A_REVERSE);
+                }
+                mvwprintw(mainWin, i+1,55,"update");
+                if(cursorX==1 & cursorY==i){
+                        wattroff(mainWin, A_REVERSE);
+                }
+
+                if(cursorX==2 & cursorY==i){
+                        wattron(mainWin, A_REVERSE);
+                }
+                mvwprintw(mainWin, i+1,62,"delete");
+                if(cursorX==2 & cursorY==i){
+                        wattroff(mainWin, A_REVERSE);
+                }
+                p= p->next;
+                wrefresh(mainWin);
         }
-        //default
-        else{
-            printf("invalid command");
+
+
+        //main loop
+        int move;
+        char out= 0;
+
+        while(1){
+                char name[45];
+                char pty[4];
+                int priority;
+                move= wgetch(mainWin);
+                switch(move){
+                        case KEY_UP:
+                                cursorY--;
+                                if(cursorY==0){
+                                        cursorY= 0;
+                                }
+                                break;
+                        case KEY_DOWN:
+                                cursorY++;
+                                if(cursorY==length(head)){
+                                        cursorY= length(head);
+                                }
+                                break;
+                        case KEY_RIGHT:
+                                cursorX++;
+                                if(cursorX==2){
+                                    cursorX= 2;
+                                }
+                                if(cursorY==0 & cursorX==1){
+                                        cursorX= 1;
+                                }
+                                break;
+                        case KEY_LEFT:
+                                cursorX--;
+                                if(cursorX==0){
+                                        cursorX= 0;
+                                }
+                                break;
+                        case 10:
+                                //quit
+                                if(cursorX==1 & cursorY==0){
+                                        out= 1;
+                                        break;
+                                }
+                                //add
+                                else if(cursorX==0 & cursorY==0){
+                                        echo();
+                                        WINDOW * addWin= newwin(6, 50, 16, 47);
+                                        refresh();
+                                        box(addWin, 0, 0);
+                                        wrefresh(addWin);
+
+                                        mvwprintw(addWin, 1, 1, "Enter task: ");
+                                        mvwprintw(addWin, 3, 1, "set priority of task: ");
+
+                                        mvwgetnstr(addWin, 2, 1, name, 45);
+                                        wrefresh(addWin);
+                                        mvwgetnstr(addWin, 4, 1, pty, 4);
+                                        wrefresh(addWin);
+                                        priority= atoi(pty);
+                                        if(priority>length(head)+1 | priority==0){
+                                                priority= length(head)+1;
+                                        }
+                                        add(priority,name,head);
+                                        werase(addWin);
+                                        wrefresh(addWin);
+                                        delwin(addWin);
+                                        noecho();
+                                }
+                                //update
+                                else if(cursorX==1  & cursorY!=0){
+                                        echo();
+                                        WINDOW * inWin= newwin(6, 50, 16, 47);
+                                        refresh();
+                                        box(inWin, 0, 0);
+                                        wrefresh(inWin);
+
+                                        mvwprintw(inWin, 1, 1,"update priorty");
+                                        mvwprintw(inWin, 3, 1, "set new priority of task: ");
+
+                                        mvwgetnstr(inWin, 4, 1, pty, 4);
+                                        wrefresh(inWin);
+
+                                        priority= atoi(pty);
+                                        if(cursorY>length(head) | priority>length(head)){}
+                                        transfer(cursorY,priority,head);
+                                        werase(inWin);
+                                        wrefresh(inWin);
+                                        delwin(inWin);
+                                        noecho();
+                                }
+                                //delete
+                                else if(cursorX==2 & cursorX!=0){
+                                     del(cursorY,head);
+                                        mvwprintw(mainWin, cursorY, 1, "                                                                     ");                                }
+                                break;
+                        default:
+                                continue;
+                                break;
+                        }
+
+                //quiting procedure
+                if(out==1){
+                        break;
+                }
+
+                struct node* p= head;
+                p= p->next;
+                if(cursorX==0 & cursorY==0){
+                        wattron(mainWin, A_REVERSE);
+                }
+                mvwprintw(mainWin, 1,55,"add");
+                if(cursorX==0 & cursorY==0){
+                        wattroff(mainWin, A_REVERSE);
+                }
+
+                if(cursorX==1 & cursorY==0){
+                        wattron(mainWin, A_REVERSE);
+                }
+                mvwprintw(mainWin, 1,62,"quit");
+                if(cursorX==1 & cursorY==0){
+                        wattroff(mainWin, A_REVERSE);
+                }
+                for(int i= 1; i<=length(head); i++){
+                        if(cursorX==0 & cursorY==i){
+                                wattron(mainWin, A_REVERSE);
+                                }
+                        mvwprintw(mainWin, i+1,1,"%s",p->name);
+                        if(cursorX==0 & cursorY==i){
+                                wattroff(mainWin, A_REVERSE);
+                        }
+
+                        if(cursorX==1 & cursorY==i){
+                                wattron(mainWin, A_REVERSE);
+                        }
+                        mvwprintw(mainWin, i+1,55,"update");
+                        if(cursorX==1 & cursorY==i){
+                                wattroff(mainWin, A_REVERSE);
+                        }
+
+                        if(cursorX==2 & cursorY==i){
+                                wattron(mainWin, A_REVERSE);
+                        }
+                        mvwprintw(mainWin, i+1,62,"delete");
+                        if(cursorX==2 & cursorY==i){
+                                wattroff(mainWin, A_REVERSE);
+                        }
+                        p= p->next;
+                        wrefresh(mainWin);
+                }
         }
-    printf("\n");
-    }
+        endwin();
+        return 0;
 }
